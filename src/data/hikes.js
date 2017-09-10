e2eb9153469970522d045fabc63bd79bcd2018d9
mccullochk <kaileen.mcculloch@gmail.com>
2017-09-10 12:43:42 -0700

    Push data to file

---
+++ src/data/hikes.js
@@ -0,0 +1,34 @@
+const hikes = [
+  {
+    id: 1,
+    name: "Kenna Cartwright",
+    duration: 90,
+    level: "Beginner",
+    coolness: 11,
+    description: "Kenna Cartwright offers an extensive network of gentle nature trails for hiking or cycling with panoramic views of the city, the Thompson Valley, Kamloops Lake and the convergence of the North and South Thompson Rivers. This combined with our distinctive 4 seasons and easy access, makes Kenna Cartwright a park for all seasons.",
+    image: "https://github.com/mccullochk/trail-buddies/blob/master/src/imgs/IMG_9841.jpg",
+    buddies: [1,2,3,4]
+  },
+  {
+    id: 2,
+    name: "Cinnamon Ridge",
+    duration: 180,
+    level: "Moderate",
+    coolness: 8,
+    description: "This province has many natural wonders--the Hoodoos are definitely among the most exotic ones. More than 10,000 years ago, the clay benches along the South Thompson River formed the bottom and shoreline of glacial Lake Thompson. Centuries of erosion have created these intriguing rock formations, referred to as Hoodoos, only a short distance from the center of Kamloops. The valley in which these majestic sandstone structures are located is easily accessible and can be reached in less than an hour. Take your camera, it’s well worth recording this example of Mother Nature’s wonders.",
+    image: "https://github.com/mccullochk/trail-buddies/blob/master/src/imgs/IMG_9820.jpg",
+    buddies: [1]
+  },
+  {
+    id: 3,
+    name: "Lonepine Valley",
+    duration: 240,
+    level: "Moderate",
+    coolness: 5,
+    description: "A relaxing and enjoyable walk through a beautiful valley floor. Walk past the tree line to a scenic view of Kamloops and the Thompson River.",
+    image: "https://github.com/mccullochk/trail-buddies/blob/master/src/imgs/summer-3.jpg",
+    buddies: [1,2]
+  }
+]
+
+export default hikes
