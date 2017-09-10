bb142786b26ba261e86a58a690a35989672b23af
mccullochk <kaileen.mcculloch@gmail.com>
2017-09-10 14:06:57 -0700

    Move css to separate file

--- src/pages/Trail.js
+++ src/pages/Trail.js
@@ -7,6 +7,8 @@ import CardTitle from 'react-toolbox/lib/card/CardTitle'
 import FontIcon from 'react-toolbox/lib/font_icon';
 import {Link} from 'react-router-dom';
 
+import '../css/Trail.css'
+
 import hikes from '../data/hikes.js'
 import buddies from '../data/buddies.js'
 
@@ -65,85 +67,6 @@ class Trail extends Component {
           )
         })}
         </div>
-        <style>{`
-          .contact {
-            display: flex;
-            margin: 0 auto;
-            margin-bottom: 20px;
-          }
-          .contact .contact__img {
-            height: 150px;
-            width: calc((100%-20px)*0.5);
-          }
-          .contact .contact__info {
-            height: 150px;
-            width: calc((100%-20px)*0.5);
-          }
-          .contact__info--body {
-            margin-left: 10px;
-          }
-          .contact__info--body h2 {
-            font-size: 20px;
-          }
-          .contact__info--body span {
-            position: relative;
-            top: 2px;
-            font-size: 15px;
-          }
-          .contact__info--body .book {
-            margin-left: 20px;
-          }
-          .description {
-            margin-bottom: 20px;
-          }
-          .buddies {
-            display: flex;
-            flex-wrap: wrap;
-          }
-          .buddies__cards {
-            width: 154px;
-            height: 100px;
-            margin-right: 5px;
-            margin-bottom: 10px;
-          }
-          .buddies .buddies__cards p {
-            font-size: 12px;
-          }
-          h2 {
-            margin: 10px 0 10px 0;
-          }
-          p {
-            margin: 0;
-            margin-bottom: 10px;
-          }
-          .buddies_cards--title div:last-child {
-            width: 50%;
-          }
-          body div.buddies_cards--title div:last-child h5,
-          body div.buddies_cards--title div:last-child p {
-            font-size: 10px !important;
-          }
-          body div.buddies_cards--title div:last-child h5 {
-            color: black !important;
-          }
-          .buddies_cards span {
-            color: black !important;
-          }
-          .buddies a {
-            text-decoration: none !important;
-          }
-          .buddies_cards--reviews span  {
-            font-size: 16px;
-            color: black !important;
-          }
-          .buddies_cards--reviews {
-            margin-left: 15px;
-            margin-top: 7px;
-          }
-          div.buddies_cards--title {
-            padding-bottom: 10px !important;
-          }
-        `}</style>
       </div>
 
     )
